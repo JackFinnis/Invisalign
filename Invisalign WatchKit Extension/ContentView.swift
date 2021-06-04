baa535ae929ad490e95ff4128735aed815d197ee
Jack <finnis.j@etoncollege.org.uk>
2021-06-04 13:57:37 +0100

    Initial Commit

---
+++ Invisalign WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Invisalign WatchKit Extension
+//
+//  Created by Finnis on 04/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Invisalign WatchKit Extension/InvisalignApp.swift
@@ -0,0 +1,19 @@
+//
+//  InvisalignApp.swift
+//  Invisalign WatchKit Extension
+//
+//  Created by Finnis on 04/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct InvisalignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
